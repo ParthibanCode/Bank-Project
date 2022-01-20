f448bf37549a20f1c418f81847735c9492528384
ParthibanCode <parthlearningg@gmail.com>
2022-01-20 16:21:21 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,87 @@
+package com.example.demo.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Customer {
+	private Integer id;
+	private String name;
+	private char gender;
+	private Date dob;
+	private String address;
+	private String city;
+	private String state;
+	private int pin;
+	private int phno;
+	private int fax;
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	public int getPhno() {
+		return phno;
+	}
+	public void setPhno(int phno) {
+		this.phno = phno;
+	}
+	public int getFax() {
+		return fax;
+	}
+	public void setFax(int fax) {
+		this.fax = fax;
+	}
+
+}
